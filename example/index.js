4359cf4faf2947bba56f4f6d8b73442620e003ed
Samuel Reed <samuel.trace.reed@gmail.com>
2017-09-11 11:11:08 -0500

    Initial Commit

---
+++ example/index.js
@@ -0,0 +1,37 @@
+'use strict';
+var Limiter = require('../');
+
+var t = new Limiter();
+var results = [];
+
+// add jobs using the familiar Array API
+t.push(function(cb) {
+  results.push('two');
+  cb();
+});
+
+t.push(
+  function(cb) {
+    results.push('four');
+    cb();
+  },
+  function(cb) {
+    results.push('five');
+    cb();
+  }
+);
+
+t.unshift(function(cb) {
+  results.push('one');
+  cb();
+});
+
+t.splice(2, 0, function(cb) {
+  results.push('three');
+  cb();
+});
+
+// begin processing, get notified on end
+t.start(function() {
+  console.log('all done:', results);
+});
