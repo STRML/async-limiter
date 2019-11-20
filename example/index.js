e17c82fa3084f1ffe757162e9de4b2486fed6800
Samuel Reed <samuel.trace.reed@gmail.com>
2019-11-20 08:34:49 -0800

    fix(example): start() method does not exist
    Fixes #5

--- example/index.js
+++ example/index.js
@@ -1,37 +1,40 @@
 'use strict';
 var Limiter = require('../');
 
-var t = new Limiter();
+var t = new Limiter({ concurrency: 1 });
 var results = [];
 
 // add jobs using the familiar Array API
 t.push(function(cb) {
-  results.push('two');
-  cb();
+  results.push('one');
+  process.nextTick(cb);
 });
 
 t.push(
   function(cb) {
     results.push('four');
-    cb();
+    process.nextTick(cb);
   },
   function(cb) {
     results.push('five');
-    cb();
+    process.nextTick(cb);
   }
 );
 
+// While this would normally push to the beginning of the array,
+// processing has already started so 'one' is in flight.
+// The async function is initiated synchronously (but resolves asynchronously)
+// unless we've hit our parallelism limit.
 t.unshift(function(cb) {
-  results.push('one');
-  cb();
+  results.push('two');
+  process.nextTick(cb);
 });
 
-t.splice(2, 0, function(cb) {
+t.splice(1, 0, function(cb) {
   results.push('three');
-  cb();
+  process.nextTick(cb);
 });
 
-// begin processing, get notified on end
-t.start(function() {
+t.onDone(function() {
   console.log('all done:', results);
 });
